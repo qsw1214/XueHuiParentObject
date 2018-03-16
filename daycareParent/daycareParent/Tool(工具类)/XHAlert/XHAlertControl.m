2e7f90a556bab6f3bae79738b0d8b6d78008a565
姚立志 <yaolizhi@aliyun.com>
2018-03-16 10:47:58 +0800

    解决冲突

--- daycareParent/daycareParent/Tool(工具类)/XHAlert/XHAlertControl.m
+++ daycareParent/daycareParent/Tool(工具类)/XHAlert/XHAlertControl.m
@@ -53,13 +53,23 @@
 //     }];
     
     
-    self.alertBoard.layer.position = self.center;
-    self.alertBoard.transform = CGAffineTransformMakeScale(0.60, 0.60);
-    [UIView animateWithDuration:0.25 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:1 options:UIViewAnimationOptionCurveLinear animations:^{
-        self.alertBoard.transform = CGAffineTransformMakeScale(1.0, 1.0);
-    } completion:^(BOOL finished)
-    {
+//    self.alertBoard.layer.position = self.center;
+//    self.alertBoard.transform = CGAffineTransformMakeScale(0.60, 0.60);
+//    [UIView animateWithDuration:0.25 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:1 options:UIViewAnimationOptionCurveLinear animations:^{
+//        self.alertBoard.transform = CGAffineTransformMakeScale(1.0, 1.0);
+//    } completion:^(BOOL finished)
+//    {
+//    }];
+    
+    
+    
+    self.alertBoard.alpha = 0;
+    self.alertBoard.transform = CGAffineTransformScale(self.transform,0.1,0.1);
+    [UIView animateWithDuration:AlertTime animations:^{
+        self.alertBoard.transform = CGAffineTransformIdentity;
+        self.alertBoard.alpha = 1;
     }];
+    
 }
 
 

563b2ea06cc01aa229e68f89b5200fd8583f9004
yueluowusheng <lipengfei0114@163.com>
2018-04-09 16:29:20 +0800

    更改配置

--- daycareParent/daycareParent/消息/聊天/view/XHRCTableViewCell.h
+++ daycareParent/daycareParent/消息/聊天/view/XHRCTableViewCell.h
@@ -6,9 +6,9 @@
 //  Copyright © 2018年 XueHui. All rights reserved.
 //
 
-#import <RongIMKit/RongIMKit.h>
+#import <RongIMLib/RongIMLib.h>
 
 @interface XHRCTableViewCell : ParentTableViewCell
 
--(void)setItemObject:(RCConversationModel *)model;
+-(void)setItemObject:(RCConversation *)model;
 @end
